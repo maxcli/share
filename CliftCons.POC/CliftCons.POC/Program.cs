238a7d5a2ea3428c031b7d7342f1a14af5724dfd
peter clift <Peter.clift@gmail.com>
2015-10-24 09:02:25 -0700

    peter01

---
+++ CliftCons.POC/CliftCons.POC/Program.cs
@@ -0,0 +1,79 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Web.Script.Serialization;
+using System.Xml;
+using System.Xml.Linq;
+
+//          https://nothere.visualstudio.com/DefaultCollection/_git/BlockChain
+//          https://toshi.io/docs/#introduction  
+//          http://json2csharp.com/#    , and paste in https://bitcoin.toshi.io/api/v0/blocks to get poco 
+
+namespace CliftCons.POC
+{
+    class Program
+    {
+        const string uri = @"https://bitcoin.toshi.io/api/v0/blocks";
+
+        static void Main(string[] args)
+        {
+
+
+            HttpStatusCode statusCode;
+            HttpWebRequest request;
+            HttpWebResponse response;
+            //Document responseBody;
+
+            request = GetRequest(uri);
+            try
+            {
+                response = (HttpWebResponse)request.GetResponse();
+            }
+            catch (WebException ex)
+            {
+                response = (HttpWebResponse)ex.Response;
+            }
+            statusCode = response.StatusCode;
+
+            if (statusCode.Equals(HttpStatusCode.OK))
+            {
+
+
+
+                using (var reader = new StreamReader(response.GetResponseStream()))
+                {
+                    JavaScriptSerializer js = new JavaScriptSerializer();
+                    js.MaxJsonLength = Int32.MaxValue; // Whatever max length you want here
+                    var objText = reader.ReadToEnd();
+
+
+                    List<RootObject> rootObjects = js.Deserialize<List<RootObject>>(objText);
+
+                    int i = 0;
+                }
+
+            }
+            response.Close();
+
+        }
+
+        //
+        //		ContentType	"application/json" 
+        //      Not  "application/xml"
+        public static HttpWebRequest GetRequest(string url)
+        {
+
+            Uri uri = new Uri(url);
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(uri);
+            request.Method = "GET";
+            return request;
+
+        }
+    }
+}
---
+++ CliftCons.POC/CliftCons.POC/RootObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CliftCons.POC
+{ 
+    public class RootObject
+    {
+        public string hash { get; set; }
+        public string branch { get; set; }
+        public string previous_block_hash { get; set; }
+        public List<object> next_blocks { get; set; }
+        public int height { get; set; }
+        public int confirmations { get; set; }
+        public string merkle_root { get; set; }
+        public string time { get; set; }
+        public string created_at { get; set; }
+        public object nonce { get; set; }
+        public int bits { get; set; }
+        public double difficulty { get; set; }
+        public object reward { get; set; }
+        public int fees { get; set; }
+        public object total_out { get; set; }
+        public int size { get; set; }
+        public int transactions_count { get; set; }
+        public int version { get; set; }
+        public List<string> transaction_hashes { get; set; }
+    }
+}
